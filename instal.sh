0bf17ff02f1ae4ae290a84f9dd8d29a594de0c5b
Eduardo Sell Gonçalves <sell.eduardo@gmail.com>
2026-04-13 23:10:23 -0300

    ssh and bash config

--- instal.sh
+++ instal.sh
@@ -4,7 +4,9 @@ echo "Installing dotfiles..."
 
 cp git/gitconfig ~/.gitconfig
 mkdir -p ~/.ssh
+cp ssh/config ~/.ssh/config
 cp bash/bashrc ~/.bashrc
 cp bash/bash_aliases ~/.bash_aliases
+cp bash/bash_profiles ~/.bash_profiles
 
 echo "Done."
