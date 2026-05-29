397b59bced9e6fab071a6d99fba7aec599aed6cf
Eduardo Sell Gonçalves <sell.eduardo@gmail.com>
2026-05-29 16:25:05 -0300

    COMSOL fix script. It works

--- comsol/README.md
+++ comsol/README.md
@@ -1,9 +1,7 @@
-# Vendored COMSOL Linux Fix Script
+# COMSOL Linux Fix Script
 
-This directory contains a vendored third-party script used to improve
+This directory contains a script based on third-party script used to improve
 COMSOL compatibility and behavior on Linux systems.
-
-## Origin
 
 Source repository:
 
@@ -12,13 +10,3 @@ https://github.com/atomspring/COMSOL_Linux_Improvements
 Original file:
 
 comsol_fixup.sh
-
-## Updating
-
-To update the vendored version:
-
-```bash
-curl -L \
-  https://raw.githubusercontent.com/atomspring/COMSOL_Linux_Improvements/master/comsol_fixup.sh \
-  -o comsol/comsol_fixup.sh
-```
--- comsol/comsol_fixup.sh
+++ comsol/comsol_fixup.sh
@@ -1,11 +1,53 @@
 #!/bin/bash
 
-## Set file icon for MPH files and make them open with COMSOL in spite of being a zip file via globs
+############################################################################### 
+# COMSOL Linux desktop integration fix 
+# 
+# This script: 
+# - registers .mph MIME type 
+# - creates a COMSOL desktop entry 
+# - associates .mph files with COMSOL 
+# - enables double-click opening of .mph files 
+# 
+############################################################################### 
 
-#### NOTE: Pay attention to where the MPH file icon comes from.
-####       I assumed you make a symbolic link from the actual COMSOL install to /usr/sbin or something like it in PATH,
-####       this way the script will be version agnostic.
-####       Otherwise, run "sudo ln -s /usr/local/comsol50/multiphysics/bin/comsol /usr/sbin/comsol"
+
+############################################################################### 
+# CONFIGURATION 
+############################################################################### 
+
+COMSOL_BIN="/usr/local/comsol63/multiphysics/bin/glnxa64/comsol" 
+COMSOL_ICON="/usr/local/comsol63/multiphysics/bin/glnxa64/comsol.png"
+
+DESKTOP_FILE="/usr/share/applications/comsol-multiphysics-6.3.desktop" 
+WRAPPER_SCRIPT="/usr/local/bin/comsol-open"
+
+
+############################################################################### 
+# CHECKS 
+############################################################################### 
+
+
+# if [[ ! -x "$COMSOL_BIN" ]]; then 
+#         echo "ERROR: COMSOL executable not found:" 
+#         echo " $COMSOL_BIN" 
+#         exit 1 
+# fi 
+
+# if [[ ! -f "$COMSOL_ICON" ]]; then
+#         echo "ERROR: COMSOL icon not found:" 
+#         echo " $COMSOL_ICON" 
+#         exit 1 
+# fi
+
+
+
+############################################################################### 
+# CREATE MIME TYPE 
+###############################################################################
+
+mkdir -p ~/.local/share/mime/packages
+
 
 echo "<?xml version=\"1.0\"?>
  <mime-info xmlns=\"http://www.freedesktop.org/standards/shared-mime-info\">
@@ -13,32 +55,87 @@ echo "<?xml version=\"1.0\"?>
    <comment>COMSOL MPH File</comment>
    <glob pattern=\"*.mph\"/>
   </mime-type>
- </mime-info>" > /tmp/comsol-mph.xml
+ </mime-info>" > ~/.local/share/mime/packages/comsol-mph.xml
+
+echo "Updating MIME database..." 
+
+update-mime-database ~/.local/share/mime
+sudo update-mime-database /usr/share/mime
+
+sudo xdg-mime install --mode system ~/.local/share/mime/packages/comsol-mph.xml
+
+
+############################################################################### 
+# CREATE WRAPPER SCRIPT 
+###############################################################################
+
+
+echo "Creating COMSOL launcher wrapper..." 
+
+cat <<EOF | sudo tee "$WRAPPER_SCRIPT" > /dev/null 
+#!/usr/bin/env bash 
+
+FILE="\$1" 
+
+if [[ -n "\$FILE" && -f "\$FILE" ]]; then 
+        exec "$COMSOL_BIN" -open "\$FILE" 
+else 
+        exec "$COMSOL_BIN" 
+fi 
+EOF
+
+sudo chmod +x "$WRAPPER_SCRIPT"
+
+
+############################################################################### 
+# EDIT DESKTOP ENTRY 
+###############################################################################
 
 sudo echo "[Desktop Entry]
 Name=COMSOL Multiphysics 6.3
 GenericName=COMSOL Multiphysics 6.3
 Comment=Launch COMSOL Multiphysics 6.3
-Exec=/usr/local/comsol63/multiphysics/bin/glnxa64/comsol %F
-Icon=/usr/local/comsol63/multiphysics/bin/glnxa64/comsol.png
+Exec=$WRAPPER_SCRIPT %f
+Icon=$COMSOL_ICON
 Terminal=false
 Type=Application
 MimeType=application/comsolmph;
 Categories=Engineering;Science;
 StartupNotify=true" > /usr/share/applications/comsol-multiphysics-6.3.desktop
 
-sudo xdg-mime install --mode system /tmp/comsol-mph.xml
-sudo xdg-icon-resource install --mode system --context mimetypes --size 32 /usr/local/comsol63/multiphysics/bin/glnxa64/comsol.png application-comsolmph
+
+############################################################################### 
+# INSTALL MIME ICON 
+###############################################################################
+
+echo "Installing MIME icon..."
+
+sudo xdg-icon-resource install --mode system --context mimetypes --size 32 "$COMSOL_ICON" application-comsolmph
+
+
+############################################################################### 
+# ASSOCIATE MIME TYPE 
+############################################################################### 
+
+echo "Associating .mph files with COMSOL..."
+
 xdg-mime default comsol-multiphysics-6.3.desktop application/comsolmph
-sudo update-mime-database /usr/share/mime
 
-sudo echo "#!/bin/bash
-FILE=\$1
 
-if [[ -f \$FILE ]];then
-        comsol -open \$FILE &
-else
-        comsol &
-fi
+############################################################################### 
+# UPDATE DATABASES 
+############################################################################### 
 
-exit 0
+echo "Updating desktop databases..."
+
+sudo update-desktop-database 
+sudo update-mime-database /usr/share/mime || true 
+
+
+############################################################################### 
+# DONE 
+############################################################################### 
+
+echo 
+echo "COMSOL desktop integration installed successfully."
+echo
